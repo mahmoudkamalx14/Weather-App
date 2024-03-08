6c96a270dea9d98e11f7aafc905945e441b6d96d
Mahmoud Kamal <213179+mahmoudkamalx17@users.noreply.github.com>
2024-03-08 18:25:42 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weathersearch/Cubit/weather_cubit.dart';
@@ -6,19 +8,18 @@ import 'package:weathersearch/Services/weather_services.dart';
 import 'package:weathersearch/pages/home_page.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
   WeatherModel? weather;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Provider(
-      create: (context)
-      {
+      create: (context) {
         return WeatherCubit(weatherSevices: WeatherSevices()).weatherModel;
       },
       child: const MaterialApp(
@@ -28,4 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
